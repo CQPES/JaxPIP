951745256c8c3d27cb654da7c13994380476b90f
mizu-bai <shiragawa4519@outlook.com>
2024-04-11 11:07:26 +0800

    Optimized with jit

--- jaxpip/descriptor/_pip.py
+++ jaxpip/descriptor/_pip.py
@@ -17,24 +17,52 @@ class PIPDescriptor(AbstractDescriptor):
         basis_set (Union[List[jax.Array], List[np.array], List[List[float]]]):
             Permutation invariant basis set.
         alpha (float): Range parameter of Morse-like variables.
+        with_grad (bool): Whether to calculate the gradients.
+            Defaults to false.
     """
 
     def __init__(
         self,
         basis_set: Union[List[jax.Array], List[np.array], List[List[float]]],
         alpha: float = 1.0,
+        with_grad: bool = False,
     ) -> None:
         if all([isinstance(basis, jax.Array) for basis in basis_set]):
             self.basis_set = basis_set
         else:
-            self.basis_set = [jnp.array(basis, dtype=jnp.float32)
-                              for basis in basis_set]
+            self.basis_set = [
+                jnp.array(basis, dtype=jnp.float32) for basis in basis_set
+            ]
         self.alpha = alpha
+        self.with_grad = with_grad
+
+        def _calc_p_from_xyz(xyz):
+            r = _calc_r_from_xyz(xyz)
+            m = _calc_m_from_r(r, alpha=self.alpha)
+            p = _calc_p_from_m(m, self.basis_set)
+
+            return p
+
+        def _calc_p_from_xyz_with_grad(xyz):
+            r, J_r_xyz = _calc_r_from_xyz(xyz, with_grad=True)
+            m, J_m_r = _calc_m_from_r(r, alpha=self.alpha, with_grad=True)
+            p, J_p_m = _calc_p_from_m(m, self.basis_set, with_grad=True)
+
+            J_p_r = jnp.einsum("ij,jk->ik", J_p_m, J_m_r)
+            J_p_xyz = jnp.einsum("ij,jkl->ikl", J_p_r, J_r_xyz)
+
+            return p, J_p_xyz
+
+        if self.with_grad:
+            self.kernel = jax.jit(_calc_p_from_xyz_with_grad)
+        else:
+            self.kernel = jax.jit(_calc_p_from_xyz)
 
     @staticmethod
     def from_json(
         basis_json: str,
         alpha: float = 1.0,
+        with_grad: bool = False,
     ) -> "PIPDescriptor":
         """Load PIP basis from json file.
 
@@ -42,48 +70,32 @@ class PIPDescriptor(AbstractDescriptor):
             basis_json (str): Permutation invariant basis json file.
             alpha (float): Range parameter of Morse-like variables.
                 Defaults to 1.0.
+            with_grad (bool): Whether to calculate the gradients.
+                Defaults to false.
         """
         with open(basis_json) as f:
             basis_set = json.load(f)
 
-        return PIPDescriptor(basis_set, alpha)
+        return PIPDescriptor(basis_set, alpha, with_grad)
 
     def __call__(
         self,
         xyz: jax.Array,
-        with_grad: bool = False
     ) -> Union[jax.Array, Tuple[jax.Array, jax.Array]]:
         """Calculate permutation invariant polynomial.
 
         Arguments:
             xyz (jax.Array): Cartesian coordinates `xyz` with shape (n, 3),
                 where `n` is the number of atoms, in Angstroms.
-            with_grad (bool): Whether to calculate the gradients.
-                Defaults to false.
 
         Returns:
             p (jax.Array): Permutation invariant polynomial.
             J_p_xyz (jax.Array): Jacobian matrix of permutation invariant
                 polynomial `p` with respect to Cartesian coordinates `xyz`.
                 Only be calculated when gradients are required, i.e.
-                with_grad = True.
+                self.with_grad = True.
         """
-        if not with_grad:
-            r = _calc_r_from_xyz(xyz)
-            m = _calc_m_from_r(r, alpha=self.alpha)
-            p = _calc_p_from_m(m, self.basis_set)
-
-            return p
-
-        # calculate Jacobian matrix J_p(xyz) if required
-        r, J_r_xyz = _calc_r_from_xyz(xyz, with_grad=True)
-        m, J_m_r = _calc_m_from_r(r, alpha=self.alpha, with_grad=True)
-        p, J_p_m = _calc_p_from_m(m, self.basis_set, with_grad=True)
-
-        J_p_r = jnp.einsum("ij,jk->ik", J_p_m, J_m_r)
-        J_p_xyz = jnp.einsum("ij,jkl->ikl", J_p_r, J_r_xyz)
-
-        return p, J_p_xyz
+        return self.kernel(xyz)
 
     def __repr__(self) -> str:
         """Return a string representation."""
@@ -97,7 +109,7 @@ class PIPDescriptor(AbstractDescriptor):
 
 
 if __name__ == "__main__":
-    water_xyz = jnp.asarray([
+    water_xyz = jnp.array([
         [+0.00000000, +0.78383672, +0.44340501],  # H
         [+0.00000000, -0.78383672, +0.44340501],  # H
         [+0.00000000, +0.00000000, -0.11085125],  # O
@@ -115,12 +127,13 @@ if __name__ == "__main__":
         [[2, 0, 0]],             # r(HH)^2
     ]
 
-    ab4_pip = PIPDescriptor(
+    a2b_pip = PIPDescriptor(
         basis_set=basis_set,
         alpha=1.0,
+        with_grad=True,
     )
 
-    p, J_p_xyz = ab4_pip(water_xyz, with_grad=True)
+    p, J_p_xyz = a2b_pip(water_xyz)
 
     print(f"p(xyz) = {p}")
     print(f"Jp(xyz) = {J_p_xyz}")
